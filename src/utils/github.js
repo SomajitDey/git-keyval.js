011829e8a1fe26905c85e98b02c063feabe6fadb
Somajit Dey <73181168+SomajitDey@users.noreply.github.com>
2025-04-19 14:01:45 +0530

    Improved src/utils/github.js

--- src/utils/github.js
+++ src/utils/github.js
@@ -3,10 +3,19 @@ import { withCustomRequest } from '@octokit/graphql';
 import * as git from './git-hash.js';
 import { bytesToBase64 } from './conversions.js';
 
-export const repository = {};
+const repository = {
+  committer: {
+    // Name and email uses the same letter(s) for better compression
+    name: 'a a',
+    email: 'a@a.a',
+    date: '2025-01-01T00:00:00.000Z'
+  }
+};
 
-// Brief: Cache repository info
-export async function init ({ owner, repo, auth }) {
+repository.author = repository.committer;
+
+// Brief: Repository info
+repository.init = async function ({ owner, repo, auth }) {
   repository.owner = owner;
   repository.name = repo;
 
@@ -16,11 +25,11 @@ export async function init ({ owner, repo, auth }) {
     headers: {
       Authorization: `Bearer ${auth}`,
       'X-GitHub-Api-Version': '2022-11-28'
-    }    
-  })
+    }
+  });
 
   repository.graphql = withCustomRequest(repository.request);
-  
+
   // The following network call, being the slowest, is not awaited immediately
   const pending = repository.graphql(`
     query {
@@ -31,39 +40,33 @@ export async function init ({ owner, repo, auth }) {
       }
     }
   `)
-  .then((data) => data.repository.id)
-  
+    .then((data) => data.repository.id);
+
   repository.emptyBlob = await git.blobHash('');
 
   repository.id = await pending;
-}
-
-// Name and email uses the same letter(s) for better compression
-const committer = {
-  name: 'a a',
-  email: 'a@a.a',
-  date: '2025-01-01T00:00:00.000Z'
 };
-const author = committer;
 
 // Brief: Update empty ./value file in given branch with given bytes
 // Params: bytes <Uint8Array>
-export async function commitBlob ({ bytes, branch }) {
-  request('PUT /repos/{owner}/{repo}/contents/{path}', {
+repository.commitBlob = async function ({ bytes, branch }) {
+  repository.request('PUT /repos/{owner}/{repo}/contents/{path}', {
     branch,
     path: 'value',
     message: 'set value',
     committer,
     content: bytesToBase64(bytes),
-    sha: repository.emptyBlob,
+    sha: repository.emptyBlob
   });
-}
+};
 
-export async function updateRefs ({ refUpdates }) {
-  graphql(`
+repository.updateRefs = async function ({ refUpdates }) {
+  repository.graphql(`
     mutation {
       updateRefs(input: ${{ repositoryId: repository.id, refUpdates }}) {
       }
     }
-  `)
-}
+  `);
+};
+
+export default repository;
--- src/utils/github.test.js
+++ src/utils/github.test.js
@@ -1,18 +1,20 @@
 // Export your GitHub access-token as env var: GITHUB_PAT before running this script
 
-import * as github from './github.js';
+import repository from './github.js';
 import assert from 'assert';
+import { config } from 'dotenv';
 
-await github.init({ owner: 'SomajitDey', repo: 'git-keyval.js', auth: process.env.GITHUB_PAT });
+config(); // Sourcing .env
+await repository.init({ owner: 'SomajitDey', repo: 'git-keyval.js', auth: process.env.GITHUB_PAT });
 
 describe('Testing utils/github', () => {
   describe('init', () => {
-    it('repository node id', async () => {
-      assert.equal(github.repository.id, 'R_kgDOOUU7Ig');
-    })
-    
-    it('empty blob sha', async () => {
-      assert.equal(github.repository.emptyBlob, 'e69de29bb2d1d6434b8b29ae775ad8c2e48c5391');
-    })
-  })
-})
+    it('repository node id', () => {
+      assert.equal(repository.id, 'R_kgDOOUU7Ig');
+    });
+
+    it('empty blob sha', () => {
+      assert.equal(repository.emptyBlob, 'e69de29bb2d1d6434b8b29ae775ad8c2e48c5391');
+    });
+  });
+});
